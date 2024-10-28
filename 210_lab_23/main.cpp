886fa53da48ecb8a96924be0780280855e564511
Errant27 <temporal.cerauno@gmail.com>
2024-10-27 19:15:43 -0700

    commit 2

--- 210_lab_23/main.cpp
+++ 210_lab_23/main.cpp
@@ -1,29 +1,88 @@
 #include <iostream>
 #include <fstream>
 #include <iomanip>
+#include <random>
 #include <list>
 #include "Goat.h"
 using namespace std;
+
 const int SZ_NAMES = 200, SZ_COLORS = 25, MAX_AGE = 20;
-int select_goat(list<Goat> trip);
-void delete_goat(list<Goat> &trip);
-void add_goat(list<Goat> &trip, string [], string []);
-void display_trip(list<Goat> trip);
+
+int select_goat(list<Goat>);
+void delete_goat(list<Goat> &);
+void add_goat(list<Goat> &, string [], string []);
+void display_trip(list<Goat>);
 int main_menu();
 
 int main() {
-    srand(time(0));
-    bool again;
-    // read & populate arrays for names and colors
-    ifstream fin("names.txt");
+    bool again = 1;
+    
+    ifstream fin("name.txt");
     string names[SZ_NAMES];
     int i = 0;
     while (fin >> names[i++]);
     fin.close();
+    
     ifstream fin1("colors.txt");
     string colors[SZ_COLORS];
     i = 0;
     while (fin1 >> colors[i++]);
     fin1.close();
+    
+    list<Goat> trip;
+    
+    int c = 0;
+    while (again)
+    {
+        c = main_menu();
+        
+        if (again)
+            if (c == 1) {
+                add_goat(trip, colors, names);
+                cout << "[Goat added]" << endl;
+            }
+    }
+    
     return 0;
+}
+    
+int main_menu()
+{
+        int choice = 0;
+            cout << "----------------------------" << endl;
+            cout << "*** GOAT MANAGER 3001 ***" << endl;
+            cout << "[1] Add a goat" << endl;
+            cout << "[2] Delete a goat" << endl;
+            cout << "[3] List goats" << endl;
+            cout << "[4] Quit" << endl;
+            cout << "Choice --> ";
+            cin >> choice;
+            cout << "----------------------------" << endl;
+        
+        return choice;
+}
+
+void add_goat(list<Goat> &trip, string c[], string n[])
+{
+    string color;
+    string name;
+    int age;
+    int num;
+    
+    
+    random_device randNum;
+    uniform_int_distribution<int>range(1, 20);
+    
+    num = range(randNum);
+    name = n[num];
+    
+    num = range(randNum);
+    color = c[num];
+    
+    num = range(randNum);
+    age = num;
+    
+    Goat g(name, age, color);
+    
+    trip.push_back(g);
 }
